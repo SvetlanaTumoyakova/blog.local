c790799cb7430eab6e971b2868f17513499c3e30
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-10-24 00:38:34 +0700

    fix: fixed the error of displaying posts

--- views/post/index.tmpl.php
+++ views/post/index.tmpl.php
@@ -13,6 +13,7 @@
                                 <h5 class="card-title"><a href="<?= $post['post_id'] ?>"><?= $post['title'] ?></a>
                                 </h5>
                                 <p class="card-text"><?= $post['descr'] ?></p>
+                                <p class="card-text"><?= $post['content'] ?></p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
@@ -32,6 +33,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
     crossorigin="anonymous"></script>
+</div>
 </body>
 
 </html>
